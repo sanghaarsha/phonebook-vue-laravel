837bca228bd700246f1d5ca51d132293ef693041
sanghaarsha <sanghaarsha@gmail.com>
2025-07-02 12:00:49 +0545

    feat: add NavBar component with search and navigation links

---
+++ frontend/src/components/Partials/NavBar.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="navbar bg-base-100 shadow-sm px-4">
+      <div class="flex-1">
+        <a class="btn btn-ghost text-xl">daisyUI</a>
+      </div>
+
+      <div class="flex-none">
+        <input type="text" placeholder="Search" class="input input-bordered w-32 md:w-48" />
+      </div>
+
+      <div class="flex-none ml-4">
+        <nav class="flex gap-4">
+          <RouterLink to="/" class="btn btn-link">Home</RouterLink>
+          <RouterLink to="/about" class="btn btn-link">About</RouterLink>
+        </nav>
+      </div>
+    </div>
+  </header>
+</template>
