b4034643668e55123b0750f64be36a5123138312
sanghaarsha <sanghaarsha@gmail.com>
2025-07-02 11:58:39 +0545

    feat: integrate DaisyUI and Tailwind CSS

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,16 +1,9 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterView } from 'vue-router'
+import NavBar from './components/Partials/NavBar.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
+  <NavBar />
   <RouterView />
 </template>
