8dd52ebf26606222e618cfb4d0a850e870ea82c8
sanghaarsha <sanghaarsha@gmail.com>
2025-07-02 11:43:10 +0545

    chore: cleanup

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import AboutView from '../views/AboutView.vue'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
@@ -12,10 +13,7 @@ const router = createRouter({
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      component: AboutView,
     },
   ],
 })
