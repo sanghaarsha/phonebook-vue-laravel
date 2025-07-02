c152e3db5a668b2bb74e67506d2aae1d44ec4f80
sanghaarsha <sanghaarsha@gmail.com>
2025-07-02 12:23:45 +0545

    feat: add vite configuration for server proxy

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,4 +13,16 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000/',
+        changeOrigin: true,
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+      },
+    },
+  },
 })
